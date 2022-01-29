d3097b9aa7a37fa4752212f2c66cb4ab1750da38
ZacharyKaranja2 <98663171+ZacharyKaranja2@users.noreply.github.com>
2022-01-29 17:15:33 -0600

    Add files via upload

---
+++ allAirports.js
@@ -0,0 +1,44 @@
+//function getAllAirports() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', "https://american-airlines-tamuhack.herokuapp.com/airports/all", true)
+    request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+        //console.log(data)
+        element = document.getElementById("airports")
+        data.forEach(airport => {
+            row = element.insertRow()
+
+            code = row.insertCell(0)
+            code.innerHTML = airport.code
+
+            city = row.insertCell(1)
+            city.innerHTML = airport.city
+
+            state = row.insertCell(2)
+
+            timezone = row.insertCell(3)
+            timezone.innerHTML = airport.timezone.replace("_"," ")
+
+            latitude = row.insertCell(4)
+            latitude.innerHTML = airport.location.latitude
+
+            longitude = row.insertCell(4)
+            longitude.innerHTML = airport.location.longitude
+
+            
+            //element.insertRow(row)
+        })
+    } else {
+        console.log('error')
+    }
+    }
+
+    request.send()
+    //console.log("A")
+//}
+
+//export {getAllAirports}
